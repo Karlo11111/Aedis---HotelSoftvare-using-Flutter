f588dfcd355db145ce85556b34341dd785c2df30
Aschmenzy <128328179+Aschmenzy@users.noreply.github.com>
2023-10-21 13:52:26 +0200

    test2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +7,10 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Home asda'),
+        ),
+        body: Center());
   }
 }
