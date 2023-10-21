c893772a83a618bbbcf6bcfdc2dc5e5877265659
Aschmenzy <128328179+Aschmenzy@users.noreply.github.com>
2023-10-21 13:48:01 +0200

    test
    asdad

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:razvoj_sofvera/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
