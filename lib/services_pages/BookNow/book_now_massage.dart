457b820fda8d0487b3afc5523382c85dc0959184
Karlo11111 <97040384+Karlo11111@users.noreply.github.com>
2023-11-09 23:26:02 +0100

    Book Now Massage,
    A lot of changes, detecting if User has reached the limit of 2 appointments for the selected date. and trying to make it not be able to book anything after that,

--- lib/services_pages/BookNow/book_now_massage.dart
+++ lib/services_pages/BookNow/book_now_massage.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unnecessary_null_comparison, avoid_single_cascade_in_expression_statements, curly_braces_in_flow_control_structures, non_constant_identifier_names, avoid_print, sized_box_for_whitespace, prefer_final_fields
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class BookMassage extends StatefulWidget {
@@ -12,25 +14,35 @@ class BookMassage extends StatefulWidget {
 }
 
 class _BookMassageState extends State<BookMassage> {
+  //fetch user auth
+  User? user = FirebaseAuth.instance.currentUser;
+  //include hive
+  final myBox = Hive.box('UserInfo');
+
+  // Declare necessary variables for saving the  available slots, timesslots and cached slots
   late DateTime _selectedDate;
   Map<DateTime, List<String>>? _availableTimeSlots;
   CollectionReference _timeSlotsCollection =
-  FirebaseFirestore.instance.collection('MassageBookedTimeSlots');
+      FirebaseFirestore.instance.collection('MassageBookedTimeSlots');
   late Map<DateTime, List<String>>? _cachedTimeSlots = {};
 
+  //variable for today
+  DateTime today = DateTime.now();
+
+  // Initialize state when the widget is created
   @override
   void initState() {
     super.initState();
+    // Set the selected date to now and fetch the available time slots
     _selectedDate = DateTime.now();
     _availableTimeSlots = {};
-    _availableTimeSlots![_selectedDate] = generateAvailableTimeSlots();
-    _fetchAvailableTimeSlots(_selectedDate);
+    //_availableTimeSlots![_selectedDate] = generateAvailableTimeSlots();
   }
 
   List<String> generateAvailableTimeSlots() {
     // Create a list of available time slots from 8:00 AM to 8:00 PM, 30 minutes apart
     List<String> timeSlots = [];
-    for (int hour = 8; hour < 20; hour++) {
+    for (int hour = 8; hour < 14.5; hour++) {
       for (int minute = 0; minute < 60; minute += 30) {
         timeSlots.add(
             '${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}');
@@ -40,6 +52,7 @@ class _BookMassageState extends State<BookMassage> {
   }
 
   Future<void> bookTimeSlot(String selectedTimeSlot) async {
+    // Book a selected time slot if it's available
     if (_availableTimeSlots![_selectedDate]!.contains(selectedTimeSlot)) {
       setState(() {
         _availableTimeSlots![_selectedDate]!.remove(selectedTimeSlot);
@@ -48,8 +61,62 @@ class _BookMassageState extends State<BookMassage> {
       await _timeSlotsCollection
           .doc(_selectedDate.toLocal().toString())
           .update({
-        'timeSlots': FieldValue.arrayRemove([selectedTimeSlot])
+        //remove the booked timeslot from the array
+        'timeSlots': FieldValue.arrayRemove([selectedTimeSlot]),
       });
+      //fetching the UserBookedMassage collection
+      final userDocumentRef = FirebaseFirestore.instance
+          .collection("UsersBookedMassage")
+          .doc(user!.uid);
+
+      // Fetch the existing data for the user
+      final userDocumentSnapshot = await userDocumentRef.get();
+
+      //check if the user document already exists
+      if (userDocumentSnapshot.exists) {
+        // Get the existing appointments for the user
+        List<dynamic> existingAppointments =
+            userDocumentSnapshot.get('Appointments') ?? [];
+
+        // Filter the existing appointments for the selected date
+        int appointmentsForSelectedDate =
+            existingAppointments.where((appointment) {
+          DateTime appointmentDate =
+              (appointment['DateOfBooking'] as Timestamp).toDate();
+          return DateTime(appointmentDate.year, appointmentDate.month,
+                  appointmentDate.day) ==
+              DateTime(
+                  _selectedDate.year, _selectedDate.month, _selectedDate.day);
+        }).length;
+
+        if (appointmentsForSelectedDate < 2) {
+          // The user's document already exists, and there are fewer than 3 appointments for the selected date
+          existingAppointments.add({
+            'Name': myBox.get("username"),
+            'Time': selectedTimeSlot,
+            'DateOfBooking': _selectedDate,
+          });
+
+          // Update the user's document with the updated appointments
+          await userDocumentRef.update({'Appointments': existingAppointments});
+        } else {
+          // The user has reached the limit of 3 appointments for the selected date
+
+          print(
+              'User has reached the limit of 2 appointments for the selected date.');
+        }
+      } else {
+        // Create a new user document with the appointment
+        await userDocumentRef.set({
+          'Appointments': [
+            {
+              'Name': myBox.get("username"),
+              'Time': selectedTimeSlot,
+              'DateOfBooking': _selectedDate,
+            }
+          ],
+        });
+      }
     }
   }
 
@@ -61,10 +128,12 @@ class _BookMassageState extends State<BookMassage> {
       });
     } else {
       // Fetch data from Firestore if not in cache
-      final snapshot =
-          await _timeSlotsCollection.doc(date.toLocal().toString()).get();
-      if (snapshot.exists) {
-        final data = snapshot.data() as Map<String, dynamic>;
+      final docReference = _timeSlotsCollection.doc(date.toLocal().toString());
+      final docSnapshot = await docReference.get();
+
+      if (docSnapshot.exists) {
+        // Document already exists, fetch and cache the data
+        final data = docSnapshot.data() as Map<String, dynamic>;
         final timeSlots = List<String>.from(data['timeSlots']);
         setState(() {
           _availableTimeSlots![date] = timeSlots;
@@ -74,7 +143,7 @@ class _BookMassageState extends State<BookMassage> {
       } else {
         // If no data is available, generate time slots and store in Firestore
         final generatedTimeSlots = generateAvailableTimeSlots();
-        await _timeSlotsCollection.doc(date.toLocal().toString()).set({
+        await docReference.set({
           'timeSlots': generatedTimeSlots,
         });
         setState(() {
@@ -86,6 +155,8 @@ class _BookMassageState extends State<BookMassage> {
     }
   }
 
+  // This method is called when a day is selected in the table calendar.
+  // It updates the selected date and fetches available time slots for the selected date.
   void _onDaySelected(DateTime selectedDate, DateTime focusedDay) {
     setState(() {
       _selectedDate = selectedDate;
@@ -93,35 +164,123 @@ class _BookMassageState extends State<BookMassage> {
     _fetchAvailableTimeSlots(_selectedDate);
   }
 
+  //displaying message for errors
+  void displayAreYouSureBooking(
+      String timeSlot, Function()? onTap, Function()? onTapCancel) {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text(
+                        "Are you sure you want to book a Massage at $timeSlot"),
+                    SizedBox(height: 40),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        //CANCEL BUTTON
+                        Container(
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              border: Border.all(color: Colors.black, width: 3),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            height: 50,
+                            child: TextButton(
+                                style: ButtonStyle(
+                                    foregroundColor:
+                                        MaterialStatePropertyAll(Colors.black),
+                                    textStyle: MaterialStatePropertyAll(
+                                        TextStyle(fontSize: 15))),
+                                onPressed: onTapCancel,
+                                child: Text("No, cancel"))),
+                        SizedBox(width: 5),
+
+                        //YES IM SURE BUTTON
+                        Container(
+                            decoration: BoxDecoration(
+                              color: Colors.green,
+                              border: Border.all(color: Colors.black, width: 3),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            height: 50,
+                            child: TextButton(
+                                style: ButtonStyle(
+                                    foregroundColor:
+                                        MaterialStatePropertyAll(Colors.black),
+                                    textStyle: MaterialStatePropertyAll(
+                                        TextStyle(fontSize: 15))),
+                                onPressed: onTap,
+                                child: Text("Yes I'm sure"))),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //appbar for this screen
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade300,
+        foregroundColor: Colors.black,
+        title: Text("Book Your Massage"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      //body of the calendar and timeslots
       body: Column(
         children: [
-          SizedBox(height: 50),
           TableCalendar(
-            calendarFormat: CalendarFormat.week,
+            calendarFormat: CalendarFormat.month,
             focusedDay: _selectedDate,
-            firstDay: DateTime(2023),
+            firstDay: today,
             lastDay: DateTime(2024),
             onDaySelected: _onDaySelected,
             selectedDayPredicate: (day) {
-              return isSameDay(_selectedDate, day);
+              return _selectedDate.isAtSameMomentAs(day);
             },
+            headerStyle:
+                HeaderStyle(titleCentered: true, formatButtonVisible: false),
           ),
           Expanded(
             child: GridView.builder(
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 5,
+                crossAxisCount: 4,
               ),
               itemCount: _availableTimeSlots![_selectedDate]?.length ?? 0,
               itemBuilder: (context, index) {
+                //selected timeslot
                 final timeSlot = _availableTimeSlots![_selectedDate]![index];
-                return TextButton(
-                  onPressed: () {
-                    bookTimeSlot(timeSlot);
-                  },
-                  child: Text(timeSlot),
+                return Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 80,
+                      width: 90,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(12)),
+                      //text button representing each timeslot
+                      child: TextButton(
+                        onPressed: () {
+                          displayAreYouSureBooking(timeSlot, () {
+                            bookTimeSlot(timeSlot);
+                            Navigator.pop(context);
+                          }, () {
+                            Navigator.pop(context);
+                          });
+                        },
+                        child: Text(timeSlot),
+                      ),
+                    ),
+                  ],
                 );
               },
             ),
